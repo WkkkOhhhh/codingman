9f87ec304b720eeea2dab293f3bc83de90377b96
wangku <wangkun>
2017-01-19 17:47:47 +0800

    new file:   .metadata/.plugins/org.eclipse.core.resources/.history/65/10abf73c21de00161e77a73188801dcd 	new file:   .metadata/.plugins/org.eclipse.core.resources/.history/6c/6097bf3321de00161e77a73188801dcd 	new file:   .metadata/.plugins/org.eclipse.core.resources/.history/7/c0ab39ef21de00161e77a73188801dcd 	new file:   .metadata/.plugins/org.eclipse.core.resources/.history/74/7043026c26de00161e77a73188801dcd

---
+++ activemq_producer/src/main/java/com/codingman/www/active_producer/App.java
@@ -0,0 +1,13 @@
+package com.codingman.www.active_producer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
